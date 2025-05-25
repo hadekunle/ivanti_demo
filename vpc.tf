87fa84b460050b57e26ed1c9ae4a7b593ef29490
hadekunle <shitoheeb@gmail.com>
2025-05-25 16:52:56 -0500

    edit gha x2

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,29 @@ resource "aws_vpc" "main" {
     env  = "${var.environment}"
   }
 }
+
+resource "aws_security_group" "flask_sg" {
+  name        = "flask_app_sg"
+  description = "Security group for Flask application"
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
