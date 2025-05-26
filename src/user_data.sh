5c318372d7034a6043febaa03ce15586328d8bd2
hadekunle <shitoheeb@gmail.com>
2025-05-26 01:36:30 -0500

    Refactor user_data.sh: streamline virtual environment setup and Flask app creation

--- src/user_data.sh
+++ src/user_data.sh
@@ -5,23 +5,22 @@ apt-get update
 apt-get install -y python3-full python3-venv
 
 # Create a virtual environment
-python3 -m venv /home/ubuntu/venv
-source /home/ubuntu/venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 
 # Install Flask in the virtual environment
-/home/ubuntu/venv/bin/pip3 install flask
+venv/bin/pip3 install flask
 
 # Create and Start the Flask application
-cat << 'EOF' > /home/ubuntu/app.py
-from flask import Flask
+cd ~
+echo "from flask import Flask" > app.py
+echo "" >> app.py
+echo "app = Flask(__name__)" >> app.py
+echo "" >> app.py
+echo "@app.route('/')" >> app.py
+echo "def hello_world():" >> app.py
+echo "    return 'Hello World from Ade! \n Environment: ${environment}'" >> app.py
+echo "if __name__ == '__main__':" >> app.py
+echo "    app.run(host='0.0.0.0', debug=True)" >> app.py
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World from Ade! \n Environment: ${environment}'
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-EOF
-
-python3 /home/ubuntu/app.py
+python3 app.py
