c7a589529f9b1e75b1549d4128057e6c21f0f31f
hadekunle <shitoheeb@gmail.com>
2025-05-26 00:17:33 -0500

    Add Flask application and user data script; create environment-specific tfvars files for dev, prod, and test

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "flask_app" {
   vpc_security_group_ids = [aws_security_group.flask_sg.id]
   depends_on             = [aws_security_group.flask_sg]
 
-  user_data = file("./user_data.sh")
+  user_data = file("./src/user_data.sh")
 
   tags = {
     Name        = var.project_name
